eaf177142af58f93e00b6a7393a0b8b124b1b0f7
unknown <fratpalta@gmail.com>
2020-02-11 22:57:29 +0300

    türkçeleştirme2-index.js

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ io.on('connection',(socket)=>{
         socket.join(user.room)
 
         socket.emit('message', generateMessage('Admin','Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} giriş yaptı!`))
         io.to(user.room).emit('roomData',{
             room: user.room,
             users: getUsersInRoom(user.room)
@@ -61,7 +61,7 @@ io.on('connection',(socket)=>{
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} çıkış yaptı!`))
             io.to(user.room).emit('roomData', {
                 room: user.room,
                 users: getUsersInRoom(user.room)
